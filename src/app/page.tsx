6f0b79779f05aeb7802124a6ee8a9008326a2b78
SaumayGoel <sg0627@srmist.edu.in>
2025-03-02 02:36:48 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [, setHasStats] = useState<boolean>(false);
+  const [hasStats, setHasStats] = useState<boolean>(false);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -16,7 +16,7 @@ export default function Home() {
     <div className="flex flex-col h-screen w-screen justify-center items-center">
       <div className="flex flex-col items-center justify-center h-[40vh] w-[85vw] md:h-[50vh] md:w-[60vw] lg:h-[65vh] lg:w-[60vw] bg-gray-500/5 rounded-lg">
         <Link
-          href={"/attendance"}
+          href={hasStats ? "/attendance" : "/login"}
           className="flex flex-row justify-center items-center w-fit h-fit bg-red-300 px-5 py-3 text-2xl rounded font-semibold scale-96 active:scale-95 transition-all duration-300"
         >
           <span>Login</span>
