2f8e7268f616f0ba917b176b6c7dfafd58b63c3e
SaumayGoel <sg0627@srmist.edu.in>
2025-03-02 11:36:38 +0530

    chore: ui fixes

--- src/components/attendance/main.tsx
+++ src/components/attendance/main.tsx
@@ -34,99 +34,101 @@ export default function Main({ data }: MainProps) {
           {data.map((item, index) => (
             <div
               key={index}
-              className={`flex flex-col md:flex-row rounded-2xl bg-gray-100 ${
+              className={`flex flex-col rounded-2xl bg-gray-100 ${
                 index % 2 == 0 ? "dark:bg-[#171B26]" : "dark:bg-black"
               } items-center px-3 py-2 gap-2`}
             >
-              <div className="flex flex-col gap-2 lg:gap-4 w-full">
-                <div className="flex flex-row justify-between">
-                  <div className="flex flex-row items-center justify-items gap-2">
-                    <div
-                      className={`py-1 px-2 rounded-full text-[10px] text-xs w-fit h-fit font-extrabold ${
-                        item.Category.toLowerCase() == "theory"
-                          ? "bg-orange-300 text-black dark:bg-black dark:text-orange-400"
-                          : "bg-green-300 text-black dark:bg-black dark:text-green-400"
-                      }`}
-                    >
-                      {item.Category.charAt(0)}
+              <div className="flex flex-col md:flex-row w-full">
+                <div className="flex flex-col gap-2 lg:gap-4 w-full">
+                  <div className="flex flex-row justify-between">
+                    <div className="flex flex-row items-center justify-items gap-2">
+                      <div
+                        className={`py-1 px-2 rounded-full text-[10px] text-xs w-fit h-fit font-extrabold ${
+                          item.Category.toLowerCase() == "theory"
+                            ? "bg-orange-300 text-black dark:bg-black dark:text-orange-400"
+                            : "bg-green-300 text-black dark:bg-black dark:text-green-400"
+                        }`}
+                      >
+                        {item.Category.charAt(0)}
+                      </div>
+                      <div
+                        className={`py-1 px-2 rounded text-xs w-fit h-fit font-extrabold ${
+                          item.Category.toLowerCase() == "theory"
+                            ? "bg-orange-300 text-black dark:bg-black dark:text-orange-400"
+                            : "bg-green-300 text-black dark:bg-black dark:text-green-400"
+                        }`}
+                      >
+                        {item["Course Code"].replace("Regular", "")}
+                      </div>
                     </div>
-                    <div
-                      className={`py-1 px-2 rounded text-xs w-fit h-fit font-extrabold ${
-                        item.Category.toLowerCase() == "theory"
-                          ? "bg-orange-300 text-black dark:bg-black dark:text-orange-400"
-                          : "bg-green-300 text-black dark:bg-black dark:text-green-400"
-                      }`}
-                    >
-                      {item["Course Code"].replace("Regular", "")}
-                    </div>
-                  </div>
 
-                  <span className="flex sm:hidden">
-                    <Margin
-                      hoursConducted={parseInt(item["Hours Conducted"])}
-                      hoursAbsent={parseInt(item["Hours Absent"])}
-                      attendance={parseInt(item["Attn %"])}
-                    />
-                  </span>
-                </div>
-                <p className="text-md lg:text-xl font-semibold">
-                  {item["Course Title"]}
-                </p>
-              </div>
-
-              <div className="flex flex-row w-full justify-end">
-                <div className="flex flex-row w-full md:w-[456px] justify-between items-center">
-                  <div className="flex flex-row">
-                    <div className="flex flex-row justify-center items-center px-2 h-fit bg-gray-200 text-black dark:bg-white rounded-l-2xl text-base md:text-md font-bold w-fit gap-[5px]">
-                      <span className="rounded-[100px] bg-black text-black text-white text-xs px-[5px]">
-                        T
-                      </span>
-                      {item["Hours Conducted"]}
-                    </div>
-                    <div className="flex flex-row font-bold bg-red-200 dark:bg-[#3C1E1B] text-[#F54B2A] px-2 text-base md:text-md items-center justify-center w-fit h-fit gap-[5px]">
-                      <span className="text-red-500 rounded-[100px] bg-black text-xs px-[5px]">
-                        A
-                      </span>
-                      {item["Hours Absent"]}
-                    </div>
-                    <div className="flex flex-row justify-center items-center px-2 h-fit bg-green-200 dark:bg-[#1B3C1E] text-base md:text-md rounded-r-2xl text-green-500 dark:text-[#49DE80] font-bold w-fit gap-[5px]">
-                      <span className="text-green-500 rounded-[100px] bg-black text-xs px-[5px]">
-                        P
-                      </span>
-                      {parseInt(item["Hours Conducted"]) -
-                        parseInt(item["Hours Absent"])}
-                    </div>
+                    <span className="flex sm:hidden">
+                      <Margin
+                        hoursConducted={parseInt(item["Hours Conducted"])}
+                        hoursAbsent={parseInt(item["Hours Absent"])}
+                        attendance={parseInt(item["Attn %"])}
+                      />
+                    </span>
                   </div>
-                  <div className="hidden sm:flex">
-                    <Margin
-                      hoursConducted={parseInt(item["Hours Conducted"])}
-                      hoursAbsent={parseInt(item["Hours Absent"])}
-                      attendance={parseInt(item["Attn %"])}
-                    />
-                  </div>
-                  {parseInt(item["Attn %"]) <= 75 ? (
-                    <div className="w-[60px] h-[60px]">
-                      <Alert />
-                    </div>
-                  ) : (
-                    <div className="w-[60px] h-[60px]"></div>
-                  )}
-                  <p
-                    className={`text-[26px] md:text-3xl font-extrabold text-nowrap ${
-                      parseInt(item["Attn %"]) >= 90
-                        ? "text-green-400"
-                        : parseInt(item["Attn %"]) > 75
-                        ? "text-orange-300"
-                        : "text-red-400"
-                    }`}
-                  >
-                    {item["Attn %"]} %
+                  <p className="text-md lg:text-xl font-semibold">
+                    {item["Course Title"]}
                   </p>
+                </div>
+
+                <div className="flex flex-row w-full justify-end">
+                  <div className="flex flex-row w-full md:w-[456px] justify-between items-center">
+                    <div className="flex flex-row">
+                      <div className="flex flex-row justify-center items-center px-2 h-fit bg-gray-200 text-black dark:bg-white rounded-l-2xl text-base md:text-md font-bold w-fit gap-[5px]">
+                        <span className="rounded-[100px] bg-black text-black text-white text-xs px-[5px]">
+                          T
+                        </span>
+                        {item["Hours Conducted"]}
+                      </div>
+                      <div className="flex flex-row font-bold bg-red-200 dark:bg-[#3C1E1B] text-[#F54B2A] px-2 text-base md:text-md items-center justify-center w-fit h-fit gap-[5px]">
+                        <span className="text-red-500 rounded-[100px] bg-black text-xs px-[5px]">
+                          A
+                        </span>
+                        {item["Hours Absent"]}
+                      </div>
+                      <div className="flex flex-row justify-center items-center px-2 h-fit bg-green-200 dark:bg-[#1B3C1E] text-base md:text-md rounded-r-2xl text-green-500 dark:text-[#49DE80] font-bold w-fit gap-[5px]">
+                        <span className="text-green-500 rounded-[100px] bg-black text-xs px-[5px]">
+                          P
+                        </span>
+                        {parseInt(item["Hours Conducted"]) -
+                          parseInt(item["Hours Absent"])}
+                      </div>
+                    </div>
+                    <div className="hidden sm:flex">
+                      <Margin
+                        hoursConducted={parseInt(item["Hours Conducted"])}
+                        hoursAbsent={parseInt(item["Hours Absent"])}
+                        attendance={parseInt(item["Attn %"])}
+                      />
+                    </div>
+                    {parseInt(item["Attn %"]) <= 75 ? (
+                      <div className="w-[60px] h-[60px]">
+                        <Alert />
+                      </div>
+                    ) : (
+                      <div className="w-[60px] h-[60px]"></div>
+                    )}
+                    <p
+                      className={`text-[26px] md:text-3xl font-extrabold text-nowrap ${
+                        parseInt(item["Attn %"]) >= 90
+                          ? "text-green-400"
+                          : parseInt(item["Attn %"]) > 75
+                          ? "text-orange-300"
+                          : "text-red-400"
+                      }`}
+                    >
+                      {item["Attn %"]} %
+                    </p>
+                  </div>
                 </div>
               </div>
               <div
-                className={`gap-[3px] w-full text-xs text-black-500 dark:text-yellow-500 ${
-                  predictedButton == 1 ? "flex flex-row items-center" : "hidden"
+                className={`flex-row gap-[3px] w-full text-xs text-black-500 dark:text-yellow-500 ${
+                  predictedButton == 1 ? "flex items-center" : "hidden"
                 }`}
               >
                 <span>
