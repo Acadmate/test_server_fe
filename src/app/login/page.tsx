2ac6c325ddcb07ce8ec531e38e6131104d8f9a73
SaumayGoel <sg0627@srmist.edu.in>
2025-03-17 03:28:19 +0530

    chore: ui improvements

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 import LoginForm from "@/components/login/form";
-import Toggle from "@/components/shared/switchTheme";
+// import Toggle from "@/components/shared/switchTheme";
+import "@/components/loading.css";
 
 export default function Login() {
   return (
-    <div className="flex flex-row bg-[#171B26] rounded-lg overflow-hidden md:flex-row justify-center mx-auto mt-32 md:mt-20 lg:mt-24 w-full max-w-screen-lg">
-      <div className="flex flex-col w-full md:w-1/2 p-1 md:p-8 gap-6 md:rounded-r-lg">
-        <div className="flex flex-row justify-between items-center px-4">
-          <p className="text-3xl lg:text-4xl font-extrabold">Acadmate</p>
-          <div className="pt-1">
-            <Toggle />
+    <div className="pattern-bg flex flex-col min-h-screen w-full justify-center p-4 md:p-12">
+      <div className="flex flex-row bg-gray-300 dark:bg-[#171B26] rounded-lg overflow-hidden md:flex-row justify-center mx-auto w-full max-w-screen-lg">
+        <div className="flex flex-col w-full md:w-1/2 p-1 md:p-8 gap-6 md:rounded-r-lg">
+          <div className="flex flex-row justify-between items-center px-4">
+            <p className="text-3xl lg:text-4xl font-extrabold">Acadmate</p>
+            {/* <div className="pt-1">
+              <Toggle />
+            </div> */}
           </div>
+          <LoginForm />
         </div>
-        <LoginForm />
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,49 @@
 "use client";
-import Link from "next/link";
-// import { useState } from "react";
+import "@/components/loading.css";
+import CheckButton from "@/components/home/checkOutBtn";
 
 export default function Home() {
-  // const [hasStats, ] = useState<boolean>(false);
-
-  // useEffect(() => {
-  //   if (typeof window !== "undefined") {
-  //     const stats = localStorage.getItem("stats");
-  //     setHasStats(stats === "true");
-  //   }
-  // }, []);
-
   return (
-    <div className="flex flex-col h-screen w-screen justify-center items-center">
-      <div className="flex flex-col items-center justify-center h-[40vh] w-[85vw] md:h-[50vh] md:w-[60vw] lg:h-[65vh] lg:w-[60vw] bg-gray-500/5 rounded-lg">
-        <Link
-          href={"/login"}
-          className="flex flex-row justify-center items-center w-fit h-fit bg-red-300 px-5 py-3 text-2xl rounded font-semibold scale-96 active:scale-95 transition-all duration-300"
-        >
-          <span>Login</span>
-        </Link>
+    <div className="pattern-bg flex flex-col min-h-screen w-full justify-center p-4 md:p-12">
+      <div className="max-w-4xl mx-auto w-fit h-fit p-6 backdrop-blur-md bg-black/40 rounded-lg border border-green-400/40 shadow-lg">
+        <div className="flex flex-col">
+          <div className="flex flex-row text-[40px] lg:text-[120px] font-bold leading-none text-[#66FF65] tracking-tight">
+            Acadmate<span className="text-white">.in</span>
+          </div>
+
+          <div className="flex items-center">
+            <div className="relative flex flex-row items-center font-bold py-2">
+              <span className="relative z-10 bg-clip-text text-transparent bg-gradient-to-r from-[#66FF65] to-green-200 tracking-tight text-lg md:text-3xl lg:text-4xl mx-1">
+                #
+              </span>
+              <span className="relative z-10 bg-clip-text text-transparent bg-gradient-to-r from-[#66FF65] to-green-200 tracking-tight text-lg md:text-3xl lg:text-4xl">
+                Breaking
+              </span>
+              <span className="ml-2 relative z-10 text-white tracking-tight text-lg md:text-3xl lg:text-4xl">
+                academia
+              </span>
+              <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-20">
+                <div className="w-full h-[1px] bg-gradient-to-r from-transparent via-[#66FF65] to-transparent"></div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-row text-sm md:text-base">
+            From tracking performance to predicting attendance, we&apos;ve got
+            it all covered. Timetable? Calendar? It&apos;s all here! Along with
+            a bunch of other cool stuff.
+          </div>
+
+          <div className="pt-4">
+            <CheckButton />
+          </div>
+
+          <div className="flex flex-row items-center justify-center bg-full w-full mt-4 text-bold text-xs">
+            <p>
+              Made by <span className="text-[#66FF65]">Lil penguinz</span>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
